a87c66a03a0962921e88e64524ac9e7792df879c
Your Name <you@example.com>
2022-06-20 23:00:05 -0300

    agrego formulario y un parte de la logica de guardado de pagina a json

--- codigo.js
+++ codigo.js
@@ -1,1 +1,18 @@
-alert("hola")
+const paginas=[
+    {
+    "id":"",
+    "nombre":"http://www.google.com.ar"
+    },
+
+
+]
+
+const formulario=document.getElementById("formulario");
+const pagina=document.getElementById("otraPagina");
+
+formulario.addEventListener('submit', (e)=>{
+
+    e.preventDefault();
+    console.log(pagina.value);
+
+});
