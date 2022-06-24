71d24515b78147a8e634de243c9c7a274a55c17d
Your Name <you@example.com>
2022-06-24 18:27:00 -0300

    agrego comentarios y reacomodo el codigo

--- codigo.js
+++ codigo.js
@@ -8,53 +8,55 @@ const mostrarPaginas=document.getElementById("mostrarPaginas");
 let cardPagina=document.createElement("div")
 
 
+
 //retorna si existen paginas o no en localstorage
 localStorageVacio=()=>{
     return localStorage.getItem("paginas")==null ? true : false;
 }
 
-vacio=()=>{
+
+//funcion que se usa cada vez que se refresca la pagina
+//si hay datos en localStorage los carga de nuevo en pantalla. Asi obtengo los datos que habian sido cargados anteriormente
+actualizoDatos=()=>{
     if(paginas.length==0 && !localStorageVacio()){
    
-        obtenerPaginasStorage= JSON.parse(localStorage.getItem("paginas"));
-        
+        obtenerPaginasStorage= JSON.parse(localStorage.getItem("paginas")); 
         obtenerPaginasStorage.forEach(p=>{
             paginas.push(p)
         })
-        console.log(obtenerPaginasStorage)
-             
-        console.log(paginas)
     
-    
-     paginas.forEach(p =>{
-         cardPagina.innerHTML+= `<div> ${p.nombre} </div> `
-         mostrarPaginas.appendChild(cardPagina);
-         console.log("nombre: "+p)
-    
-    })
+        paginas.forEach(p =>{
+             cardPagina.innerHTML+= `<div> ${p.nombre} </div> `
+             mostrarPaginas.appendChild(cardPagina); 
+        })
     }
 }
 
-vacio()
-//se crea un div donde como informacion se le añade el nombre de la pagina agregada por el usuario
+
+actualizoDatos()
+
+
 //este evento sucede al apretar click en el boton "añadir" o al apretar enter
+//se crea un div donde como informacion se le añade el nombre de la pagina agregada por el usuario
 formulario.addEventListener('submit', (e)=>{
     e.preventDefault();
    
-         cardPagina.innerHTML+= `<div> ${pagina.value} </div> `
-         mostrarPaginas.appendChild(cardPagina);
-         agregarPagina(pagina.value)
+    cardPagina.innerHTML+= `<div> ${pagina.value} </div> `
+    mostrarPaginas.appendChild(cardPagina);
+    agregarPagina(pagina.value)     
 
-    // localStorageVacio ? agregarPagina():""
     // abrirPaginas()
-
 });
+
+
 
 //Agrega nombre de pagina a array paginas y guarda paginas en local storage
 agregarPagina=(p)=>{
     paginas.push({"nombre":`${p}`})
-    guardarPaginas()
+    guardarEnLocalStorage()
 }
+
+
 
 //abre todas las paginas agregadas como favoritas
 abrirPaginas=()=>{
@@ -64,8 +66,9 @@ abrirPaginas=()=>{
     })
 }
 
+
 //guarda las paginas en local storage
-guardarPaginas=()=>{
+guardarEnLocalStorage=()=>{
     localStorage.setItem("paginas",JSON.stringify(paginas));
 }
 
