ff8f5e101c3e6f6c26ed47b7c574f7b994a0382c
Your Name <you@example.com>
2022-06-24 00:44:37 -0300

    se siguen muestran los datos al refrescar la pagina

--- codigo.js
+++ codigo.js
@@ -5,18 +5,47 @@ const paginas=[]
 const formulario=document.getElementById("formulario");
 const pagina=document.getElementById("otraPagina");
 const mostrarPaginas=document.getElementById("mostrarPaginas");
+let cardPagina=document.createElement("div")
 
 
+//retorna si existen paginas o no en localstorage
+localStorageVacio=()=>{
+    return localStorage.getItem("paginas")==null ? true : false;
+}
+
+vacio=()=>{
+    if(paginas.length==0 && !localStorageVacio()){
+   
+        obtenerPaginasStorage= JSON.parse(localStorage.getItem("paginas"));
+        
+        obtenerPaginasStorage.forEach(p=>{
+            paginas.push(p)
+        })
+        console.log(obtenerPaginasStorage)
+             
+        console.log(paginas)
+    
+    
+     paginas.forEach(p =>{
+         cardPagina.innerHTML+= `<div> ${p.nombre} </div> `
+         mostrarPaginas.appendChild(cardPagina);
+         console.log("nombre: "+p)
+    
+    })
+    }
+}
+
+vacio()
 //se crea un div donde como informacion se le añade el nombre de la pagina agregada por el usuario
 //este evento sucede al apretar click en el boton "añadir" o al apretar enter
 formulario.addEventListener('submit', (e)=>{
     e.preventDefault();
-    
-    let cardPagina=document.createElement("div")
-    cardPagina.innerHTML+= `<div> ${pagina.value} </div> `
-    mostrarPaginas.appendChild(cardPagina);
-    
-    agregarPagina(pagina.value)
+   
+         cardPagina.innerHTML+= `<div> ${pagina.value} </div> `
+         mostrarPaginas.appendChild(cardPagina);
+         agregarPagina(pagina.value)
+
+    // localStorageVacio ? agregarPagina():""
     // abrirPaginas()
 
 });
@@ -40,8 +69,5 @@ guardarPaginas=()=>{
     localStorage.setItem("paginas",JSON.stringify(paginas));
 }
 
-//retorna si existen paginas o no en localstorage
-localStorageVacio=()=>{
-    return localStorage.getItem("paginas")==null ? true : false;
-}
+
 
